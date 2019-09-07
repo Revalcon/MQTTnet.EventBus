332aab3ada3a6c9af1c24a102e615eef96dd1669
Artyom Tonoyan <arttonoyan@gmail.com>
2019-09-07 04:18:20 +0400

    Add "SubscriptionInfo" class.

---
+++ MQTTnet.EventBus/MQTTnet.EventBus/SubscriptionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MQTTnet.EventBus
+{
+    public class SubscriptionInfo
+    {
+        public Type HandlerType { get; }
+        private SubscriptionInfo(Type handlerType)
+            => HandlerType = handlerType;
+        public static SubscriptionInfo Typed(Type handlerType)
+            => new SubscriptionInfo(handlerType);
+    }
+}
