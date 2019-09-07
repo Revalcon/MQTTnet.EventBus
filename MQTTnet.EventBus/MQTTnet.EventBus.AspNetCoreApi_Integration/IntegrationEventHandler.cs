8254f37ea3f866c3040728c710d3b7265f74169a
Artyom Tonoyan <arttonoyan@gmail.com>
2019-09-07 05:14:37 +0400

    Add Demo app [AspNetCoreApi_Integration].

---
+++ MQTTnet.EventBus/MQTTnet.EventBus.AspNetCoreApi_Integration/IntegrationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace MQTTnet.EventBus.AspNetCoreApi_Integration
+{
+    public class IntegrationEventHandler : IIntegrationEventHandler
+    {
+        public Task Handle(MqttApplicationMessageReceivedEventArgs args)
+        {
+            //Some action...
+            return Task.CompletedTask;
+        }
+    }
+}
