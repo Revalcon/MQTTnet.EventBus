c2bc86be30024b152d964cd2e2c766022ffe637c
Artyom Tonoyan <arttonoyan@gmail.com>
2021-05-11 13:09:07 +0400

    Add Project

---
+++ src/Extensions/MQTTnet.EventBus.Microsoft.Extensions.Logging/MicrosoftLoggerOptionsBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MQTTnet.EventBus.DependencyInjection;
+using MQTTnet.EventBus.DependencyInjection.Builder;
+using MQTTnet.EventBus.Microsoft.Extensions.Logging;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class MicrosoftLoggerOptionsBuilderExtensions
+    {
+        public static IServicesBuilder AddMicrosoftExtensionsLgger(this IServicesBuilder builder)
+            => builder.AddLogger(p => p.UseMicrosoftExtension());
+
+        public static IServicesBuilder AddMicrosoftExtensionsLgger(this IServicesBuilder builder, Action<ILoggingBuilder> configure)
+            => builder.AddLogger(p => p.UseMicrosoftExtension(configure));
+
+        public static ILoggerOptionsBuilder UseMicrosoftExtension(this ILoggerOptionsBuilder builder)
+        {
+            builder.Services
+                .AddLogging()
+                .AddEventBusLogger<MicrosoftEventBusLogger>();
+            return builder;
+        }
+
+        public static ILoggerOptionsBuilder UseMicrosoftExtension(this ILoggerOptionsBuilder builder, Action<ILoggingBuilder> configure)
+        {
+            builder.Services
+                .AddLogging(configure)
+                .AddEventBusLogger<MicrosoftEventBusLogger>();
+            return builder;
+        }
+    }
+}
