eb6516a32f911e87642c905268b8175e9fd9e712
Artyom Tonoyan <arttonoyan@gmail.com>
2021-05-13 11:16:47 +0400

    Add Newtonsoft.Json project

---
+++ src/Extensions/MQTTnet.EventBus.Newtonsoft.Json/NewtonsoftJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using MQTTnet.EventBus.Serializers;
+using Newtonsoft.Json;
+
+namespace MQTTnet.EventBus.Newtonsoft.Json
+{
+    public class NewtonsoftJsonConverter<T> : IEventConverter<T>
+    {
+        public T Deserialize(byte[] value)
+        {
+            string data = TextConvert.ToUTF8String(value);
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public byte[] Serialize(T value)
+        {
+            var data = JsonConvert.SerializeObject(value);
+            return TextConvert.ToUTF8ByteArray(data);
+        }
+    }
+}
