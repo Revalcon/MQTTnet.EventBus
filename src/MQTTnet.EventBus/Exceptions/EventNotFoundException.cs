c114f8cba74d335c46dcbc667a02c28444a5b68e
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-09-11 13:17:30 +0400

    Fix issue in start up configuration

---
+++ src/MQTTnet.EventBus/Exceptions/EventNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MQTTnet.EventBus.Exceptions
+{
+    public class EventNotFoundException : EventException
+    {
+        public EventNotFoundException(string eventName, Type eventType)
+            : base(eventName, eventType, $"The given event '{eventName}' was not present in the mapping dictionary, check the startup configuration")
+        { }
+    }
+}
